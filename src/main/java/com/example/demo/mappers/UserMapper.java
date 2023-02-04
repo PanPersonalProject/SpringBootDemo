3cbee83ef1c68bde2185f04b9f583f3b2ce357a0
Pan <panqiwork@gmail.com>
2023-02-04 13:34:53 +0800

    mybatis sql注解

--- src/main/java/com/example/demo/mappers/UserMapper.java
+++ src/main/java/com/example/demo/mappers/UserMapper.java
@@ -1,10 +1,11 @@
 package com.example.demo.mappers;
 
 import com.example.demo.bean.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface UserMapper {
-
+    @Select("select * from user")
     List<User> selectAllUser();
 }
